f9ac622495b5debfe199ed79f5eb05fc986eb7d6
Kyle Cordes <kyle.cordes@oasisdigital.com>
2022-03-22 16:36:18 -0500

    Rearrange scripts for new usage

--- tools/build-all.sh
+++ tools/build-all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 set -e
 
-for d in [0-9]*
-do
-    echo ================================================ $d
-    ( cd "$d" && ./exercise.sh )
-done
+# This is intended to be called from the directory containing all of our various
+# Bazel example repos.
+
+# ./bazel-resources/tools/build-all.sh
+
+find . -name exercise.sh -maxdepth 2 -execdir ./exercise.sh \;
--- tools/clean-all.sh
+++ tools/clean-all.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-find . -maxdepth 2 -name WORKSPACE \( -execdir bazel clean --expunge \; -o -execdir bazel shutdown \; \)
+# This is intended to be called from the directory containing all of our various
+# Bazel example repos.
+
+# ./bazel-resources/tools/clean-all.sh
+
+find . -maxdepth 2 -name WORKSPACE.bazel \( -execdir bazel clean --expunge \; -o -execdir bazel shutdown \; \)
 
 find . -maxdepth 3 -name 'bazel-*' -type l -print -delete
 
